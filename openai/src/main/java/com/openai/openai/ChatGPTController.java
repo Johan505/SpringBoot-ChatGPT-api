a875c92a52e48f5b91c833dff654c764c3d3aae7
Johan505 <ajohanmauricio@gmail.com>
2023-03-25 18:11:12 -0500

    Add Cors Configuration to Angular permisions

--- openai/src/main/java/com/openai/openai/ChatGPTController.java
+++ openai/src/main/java/com/openai/openai/ChatGPTController.java
@@ -2,7 +2,10 @@ package com.openai.openai;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,9 @@ import io.github.flashvayne.chatgpt.service.ChatgptService;
 
 @RestController
 @RequestMapping("/chatbot")
+@CrossOrigin(origins = "http://localhost:4200")
 
-public class HelloGPTController implements InitializingBean{
+public class ChatGPTController implements InitializingBean{
     
     @Autowired
     private ChatgptService chatgptService;
@@ -30,19 +34,6 @@ public class HelloGPTController implements InitializingBean{
         return chatgptService.sendMessage(message);
     }
 
-    @GetMapping("/prompt")
-    public ChatResponse prompt(@RequestParam String message){
-        Integer maxTokens = 300;
 
-        String model = "text-davinci-003";
 
-        Double temperature = 0.5;
-
-        Double topP = 1.0;
-
-        ChatRequest chatRequest = new ChatRequest(model, message, maxTokens, temperature, topP);
-        ChatResponse res = chatgptService.sendChatRequest(chatRequest);
-        System.out.println("Response was: " + res.toString());
-        return res;
-    }
 }
